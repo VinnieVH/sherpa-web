1640767c661e10cbd938d50b889a9a022b7f975f
Vincent Van Herreweghe <vincentvanherreweghe@gmail.com>
2024-02-15 22:13:33 +0100

    SHER-3 + SHER-6: handle logout + clean up routes

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,17 +1,6 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
-import { destroySession, getSession } from "~/services/session.server";
+import { authenticator } from "~/services/auth.server";
 
 export let loader = async ({ request }: LoaderFunctionArgs) => {
-  const session = await getSession(request.headers.get("Cookie"));
-  const logoutURL = new URL(process.env.AUTH0_BASE_URL! + "/logout"); // i.e https://YOUR_TENANT.us.auth0.com/v2/logout
-
-  logoutURL.searchParams.set("client_id", process.env.AUTH0_CLIENT_ID!);
-  logoutURL.searchParams.set("returnTo", process.env.AUTH0_BASE_URL!);
-
-  return redirect(logoutURL.toString(), {
-    headers: {
-      "Set-Cookie": await destroySession(session)
-    }
-  });
+  await authenticator.logout(request, { redirectTo: "/" });
 };
